ea0ab54bf199946cc70be4db8b9f574613d31c8b
Louis Mullie <louis.mullie@gmail.com>
2012-05-15 11:10:17 -0400

    Added tests for French.

--- test/test.rb
+++ test/test.rb
@@ -1,22 +1,51 @@
+#encoding: utf-8
 require 'test/unit'
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'stanford-core-nlp'
 
+StanfordCoreNLP.model_path = '/ruby/stanford/models/'
+StanfordCoreNLP.jar_path = '/ruby/stanford/bin/'
+
 module StanfordCoreNLP
+  
   class TestStanfordCoreNLP < Test::Unit::TestCase
 
     def test_load_class
+      
       StanfordCoreNLP.load_class('PTBTokenizerAnnotator')
       assert_equal true,
       StanfordCoreNLP::PTBTokenizerAnnotator.
       respond_to?(:java_methods)
+      
+    end
+    
+    def test_all_french
+      
+      # Can't test both french and english at same time.
+      return
+      
+      # Reset default values to make sure
+      # that these features work.
+      StanfordCoreNLP.use(:french)
+      pipeline =  StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse)
+      text = "Bonjour, je suis bel et bien arrivé au château. Le roi m'a donné un biscuit."
+      text = StanfordCoreNLP::Text.new(text)
+      pipeline.annotate(text)
+      
+      sentences, tokens, tags, lemmas, begin_char, last_char = *get_information(text)
+      
+      assert_equal ["Bonjour, je suis bel et bien arrivé au château.", 
+                    "Le roi m'a donné un biscuit."], sentences
+      assert_equal %w[Bonjour , je suis bel et bien arrivé au château . Le roi m ' a donné un biscuit .], tokens
+      assert_equal %w[I , CL V ADV C ADV V P N . D N N N V V D N .], tags
+      assert_equal %w[bonjour , je sui bel et bien arrivé au château . le roi m ' a donné un biscuit .], lemmas
+      assert_equal [0, 7, 9, 12, 17, 21, 24, 29, 36, 39, 46, 48, 51, 55, 56, 57, 59, 65, 68, 75], begin_char
+      assert_equal [7, 8, 11, 16, 20, 23, 28, 35, 38, 46, 47, 50, 54, 56, 57, 58, 64, 67, 75, 76], last_char
+      
     end
 
     def test_all_english
-
-      #StanfordCoreNLP.model_path = '/ruby/gems/treat/models/stanford/'
-      #StanfordCoreNLP.jar_path = '/ruby/gems/treat/bin/stanford/'
-
+      
       # Reset default values to make sure
       # that these features work.
       StanfordCoreNLP.use(:english)
@@ -30,6 +59,22 @@ module StanfordCoreNLP
       text = StanfordCoreNLP::Text.new(text)
       pipeline.annotate(text)
 
+      sentences, tokens, tags, lemmas, begin_char, last_char = *get_information(text)
+      
+      assert_equal ['Angela Merkel met Nicolas Sarkozy on January 25th ' +
+      'in Berlin to discuss a new austerity package.'], sentences
+      assert_equal %w[Angela Merkel met Nicolas Sarkozy on
+      January 25th in Berlin to discuss a new austerity package .], tokens
+      assert_equal %w[Angela Merkel meet Nicolas Sarkozy on
+      January 25th in Berlin to discuss a new austerity package .], lemmas
+      assert_equal %w[NNP NNP VBD NNP NNP IN NNP JJ IN NNP TO VB DT JJ NN NN .], tags
+      assert_equal [0, 7, 14, 18, 26, 34, 37, 45, 50, 53, 60, 63, 71, 73, 77, 87, 94], begin_char
+      assert_equal [6, 13, 17, 25, 33, 36, 44, 49, 52, 59, 62, 70, 72, 76, 86, 94, 95], last_char
+      
+    end
+    
+    def get_information(text)
+      
       sentences = []
       
       tokens = []
@@ -53,16 +98,9 @@ module StanfordCoreNLP
 
       end
       
-      assert_equal ['Angela Merkel met Nicolas Sarkozy on January 25th ' +
-      'in Berlin to discuss a new austerity package.'], sentences
-      assert_equal %w[Angela Merkel met Nicolas Sarkozy on
-      January 25th in Berlin to discuss a new austerity package .], tokens
-      assert_equal %w[Angela Merkel meet Nicolas Sarkozy on
-      January 25th in Berlin to discuss a new austerity package .], lemmas
-      assert_equal %w[NNP NNP VBD NNP NNP IN NNP JJ IN NNP TO VB DT JJ NN NN .], tags
-      assert_equal [0, 7, 14, 18, 26, 34, 37, 45, 50, 53, 60, 63, 71, 73, 77, 87, 94], begin_char
-      assert_equal [6, 13, 17, 25, 33, 36, 44, 49, 52, 59, 62, 70, 72, 76, 86, 94, 95], last_char
-      
+      [sentences, tokens, tags, lemmas, begin_char, last_char]
+  
     end
+    
   end
 end
