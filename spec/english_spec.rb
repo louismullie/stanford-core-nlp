8cf1b51a7ef188381dd58395e3af7a9de231657f
Louis Mullie <louis.mullie@gmail.com>
2012-12-20 15:27:12 -0500

    Add specs for NER and coreference resolution.

--- spec/english_spec.rb
+++ spec/english_spec.rb
@@ -11,18 +11,20 @@ describe StanfordCoreNLP do
       StanfordCoreNLP.set_model('pos.model', 'english-left3words-distsim.tagger')
       StanfordCoreNLP.set_model('parse.model', 'englishPCFG.ser.gz')
       text = 'Angela Merkel met Nicolas Sarkozy on January 25th in Berlin to discuss a new austerity package.'
-      pipeline = StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse)
+      pipeline = StanfordCoreNLP.load(:tokenize, :ssplit, :pos, :lemma, :parse, :ner, :dcoref)
       text = StanfordCoreNLP::Annotation.new(text)
       pipeline.annotate(text)
       
-      sentences, tokens, tags, lemmas, begin_char, last_char = *get_information(text)
+      sentences, tokens, tags, lemmas, begin_char, last_char, name_tags, coref_ids = *get_information(text, true, true)
+
       sentences.should eql ['Angela Merkel met Nicolas Sarkozy on January 25th in Berlin to discuss a new austerity package.']
       tokens.should eql %w[Angela Merkel met Nicolas Sarkozy on January 25th in Berlin to discuss a new austerity package .]
       lemmas.should eql %w[Angela Merkel meet Nicolas Sarkozy on January 25th in Berlin to discuss a new austerity package .]
       tags.should eql %w[NNP NNP VBD NNP NNP IN NNP JJ IN NNP TO VB DT JJ NN NN .]
       begin_char.should eql [0, 7, 14, 18, 26, 34, 37, 45, 50, 53, 60, 63, 71, 73, 77, 87, 94]
       last_char.should eql [6, 13, 17, 25, 33, 36, 44, 49, 52, 59, 62, 70, 72, 76, 86, 94, 95]
-      
+      name_tags.should eql ["PERSON", "PERSON", "O", "PERSON", "PERSON", "O", "DATE", "DATE", "O", "LOCATION", "O", "O", "O", "O", "O", "O", "O"]
+      coref_ids.should eql ["", "1", "", "", "5", "", "3", "", "", "4", "", "", "", "", "", "6", ""]
     end
     
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,15 @@ require 'rspec'
 
 require_relative '../lib/stanford-core-nlp'
 
-# StanfordCoreNLP.model_path = '/ruby/stanford-core-nlp-min/' # Local testing only
-# StanfordCoreNLP.jar_path = '/ruby/stanford-core-nlp-min/'   # Local testing only.
+StanfordCoreNLP.model_path = '/ruby/stanford-core-nlp-minimal/' # Local testing only
+StanfordCoreNLP.jar_path = '/ruby/stanford-core-nlp-minimal/'   # Local testing only.
 
-def get_information(text)
+def get_information(text, with_name_tag=false, with_coref=false)
 
   sentences, tokens, tags, = [], [], []
   lemmas, begin_char, last_char = [], [], []
-
+  name_tags, coref_ids = [], []
+  
   text.get(:sentences).each do |sentence|
     sentences << sentence.to_s
     sentence.get(:tokens).each do |token|
@@ -18,12 +19,12 @@ def get_information(text)
       last_char << token.get(:character_offset_end).to_s.to_i
       tags << token.get(:part_of_speech).to_s
       lemmas << token.get(:lemma).to_s
-      # name_tags << token.get(:named_entity_tag).to_s
-      # coref_ids << token.get(:coref_cluster_id).to_s
+      name_tags << token.get(:named_entity_tag).to_s if with_name_tag
+      coref_ids << token.get(:coref_cluster_id).to_s if with_coref
     end
 
   end
 
-  [sentences, tokens, tags, lemmas, begin_char, last_char]
+  [sentences, tokens, tags, lemmas, begin_char, last_char, name_tags, coref_ids]
 
 end
