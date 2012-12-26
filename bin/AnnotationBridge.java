a18ac55f49905f61dcfb6366577e56cc34e8fada
Louis Mullie <louis.mullie@gmail.com>
2012-12-26 15:40:43 -0500

    Update for Java 7 compatibility.

--- bin/AnnotationBridge.java
+++ bin/AnnotationBridge.java
@@ -1,12 +1,21 @@
 import edu.stanford.nlp.ling.CoreAnnotation;
 import edu.stanford.nlp.util.ArrayCoreMap;
+import java.util.Properties;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
+// javac -cp '.:stanford-corenlp.jar' AnnotationBridge.java
+// jar cf bridge.jar AnnotationBridge.class
 public class AnnotationBridge {
     
     public static Object getAnnotation(Object entity, String name) throws ClassNotFoundException {
-    	Class<CoreAnnotation> klass;
-    	klass = (Class<CoreAnnotation>) Class.forName(name);
-    	Object object = ((ArrayCoreMap) entity).get(klass);
-		return object;
+      Class<CoreAnnotation> klass;
+      klass = (Class<CoreAnnotation>) Class.forName(name);
+      Object object = ((ArrayCoreMap) entity).get(klass);
+      return object;
+    }
+    
+    public static Object getPipelineWithProperties(Properties properties) {
+      StanfordCoreNLP pipeline = new StanfordCoreNLP(properties);
+      return pipeline;
     }
 }
