97d41eb460b63eb25b96dd24135fe89087b218ed
Louis Mullie <louis.mullie@gmail.com>
2012-12-26 16:41:40 -0500

    More Java6 BC tweaks.

--- bin/AnnotationBridge.java
+++ bin/AnnotationBridge.java
@@ -3,6 +3,7 @@ import edu.stanford.nlp.util.ArrayCoreMap;
 import java.util.Properties;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 
+// export JAVA_HOME='/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home'
 // javac -cp '.:stanford-corenlp.jar' -source 1.6 -target 1.6 AnnotationBridge.java
 // jar cf bridge.jar AnnotationBridge.class
 public class AnnotationBridge {
